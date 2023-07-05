425ef40a8e056a705a56a0a324b4a30809b42eba
unknown <jonas.gastro91@gmail.com>
2023-07-05 18:37:54 -0300

    feat:base project

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function App() {
+
+  return (
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          LINK PARA VITE PAGE
+        </a>
+      </div>
+  )
+}
+
+export default App
