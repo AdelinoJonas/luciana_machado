47c2d2a10669be18fb5c6778a0bd17d92d52d1ff
unknown <jonas.gastro91@gmail.com>
2023-07-06 22:41:07 -0300

    feat: home responsive

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import wave1 from '../../assets/waves/home/wave1.svg';
-import wave2 from '../../assets/waves/home/wave2.svg';
+import wave1 from '../../assets/waves/home/colorwave-bg.png';
 import wave3 from '../../assets/waves/home/wave3.svg';
+// import wave3 from '../../assets/waves/home/wave3.svg';
 import './style.css';
 
 export default function Home() {
   return (
     <div className='container'>
-      <div className="wave">
-        <img src={wave1} alt="background wave" className='wave1'/>
-        <img src={wave2} alt="background wave" className='wave2'/>
-        <img src={wave3} alt="background wave" className='wave3'/>
-      </div>
+      <img src={wave1} alt="background wave" className='wave1'/>
+      <img src={wave3} alt="background wave" className='wave2'/>
     </div>
   )
 }
