5e6f5d27516999416e6ab59a6f2a87b32528ff0c
unknown <jonas.gastro91@gmail.com>
2023-07-10 13:26:17 -0300

    feat: about responsive

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -11,7 +11,9 @@ export default function About() {
           <p className="textDescription">
             Sou uma nutricionista apaixonada pela área da nutrição clínica e pela estética, que entendeu que alimentação e procedimentos estéticos podem trazer resultados incríveis para quem quer emagrecer de forma saudável e duradoura, tratando a pele além de garantir um envelhecimento adequado com beleza e bem-estar. Atendo ao público adulto em geral que necessite de mudanças nos hábitos alimentares e principalmente atendimento nutricional na Estética e Saúde da Mulher. Com planos alimentares personalizados.
           </p>
-          <button className="contactButton">Entre em contato</button>
+          <button className="contactButton">
+            Entre em contato
+          </button>
 
         </div>
         <div className="imgBox">
