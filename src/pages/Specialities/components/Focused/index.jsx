b1591663ac2355d6e4fa4e775d514898d588b017
unknown <jonas.gastro91@gmail.com>
2023-08-08 16:15:47 -0300

    correction

--- src/pages/Specialities/components/Focused/index.jsx
+++ src/pages/Specialities/components/Focused/index.jsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import React from "react";
 import dna from "../../../../assets/photos/dnaFood.jpg";
 import fitxfat from "../../../../assets/photos/gorduraxmusculo.jpg";
 import stetic from "../../../../assets/photos/stetic.png";
